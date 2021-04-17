a955508d155fbc05ba7b8a0263ef74d77a573e1e
Feargal <75866975+feargaldowney1@users.noreply.github.com>
2021-04-17 15:24:22 +0100

    Option 1 complete

--- runners_project.py
+++ runners_project.py
@@ -7,37 +7,49 @@ Group:          Comp 1CX
 Date:           13/04/2021
 """
 
+
 def display_menu():
+    print("                   User options                         ")
     print("---------------------------------------------------------")
     print("1. Show the results for a race")
     print("2. Add results for a race")
     print("3. Show all competitors by county")
     print("4. Show the winner of eachrace")
-    print("5.Show all the race times for one competitor")
+    print("5. Show all the race times for one competitor")
     print("6. Show all competitors who have won a race")
     print("7. Quit")
     print("---------------------------------------------------------")
     user_input = int(input("Please enter your choice from 1 - 7 >>> "))
     return user_input
 
-def choice(user_input):
-    if user_input == 1:
-        race_choice = int(input(("Which race would you like the results of? >>> "))
-    #     if race_choice == 1:
-    #         print("clonakilty")
-    #     elif race_choice == 2:
-    #         print(bandon)
-    #     else: 
-    #         print("Uh Oh, Incorrect data entered, please enter '1' or '2'.")
-    # elif user_input == 2:
+def option1(number):
+    if number == 1:
+        print("The races are the following:")
+        print("-" * 15)
+        print("1. Clonakilty")
+        print("2. Bandon")
+        print("-" * 15)
+        race_choice = int(input("Which number race would you like the results of? >>> "))
+        if race_choice == 1:
+            print("Clonakilty")
+        elif race_choice == 2:
+            print("Bandon")
+        else: 
+            print()
+            print("Uh Oh, Incorrect data entered, please enter '1' or '2'.")
+            print()
+
+    # elif number == 2:
     #     print("Adding results for a race...")
-    #     race_name = input("Where will this race take place? >>> ")
+    #     race_name = input("Where did this race take place? >>> ")
+    #     No. racers = int(input("How many racers where in the race?"))
         
 
-
+    
 def main():
-    user_input = display_menu()
-    choice(user_input)
+    number = display_menu()
+    print(number)
+    option1(number)
 
 
 main()
