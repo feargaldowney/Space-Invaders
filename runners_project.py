110c7d9812e7fde3bd1a2b351b9dbc2ed8da662c
Feargal <75866975+feargaldowney1@users.noreply.github.com>
2021-04-19 01:55:38 +0100

    added print dash function

--- runners_project.py
+++ runners_project.py
@@ -6,11 +6,14 @@ Student ID:     R00202629
 Group:          Comp 1CX
 Date:           13/04/2021
 """
+def print_dashes(i):
+    print("-" * i)
 
 
-def display_menu():
-    print("                   User options                         ")
-    print("---------------------------------------------------------")
+
+def display_menu(print_dashes):
+    print("                   User options                          ")
+    print_dashes(50)
     print("1. Show the results for a race")
     print("2. Add results for a race")
     print("3. Show all competitors by county")
@@ -18,17 +21,17 @@ def display_menu():
     print("5. Show all the race times for one competitor")
     print("6. Show all competitors who have won a race")
     print("7. Quit")
-    print("---------------------------------------------------------")
+    print_dashes(50)
     user_input = int(input("Please enter your choice from 1 - 7 >>> "))
     return user_input
 
-def option1(number):
+def option1(number, print_dashes):
     if number == 1:
         print("The races are the following:")
-        print("-" * 15)
+        print_dashes(15)
         print("1. Clonakilty")
         print("2. Bandon")
-        print("-" * 15)
+        print_dashes(15)
         race_choice = int(input("Which number race would you like the results of? >>> "))
         if race_choice == 1:
             clonakilty = open("clonakilty.txt", "r")
@@ -40,20 +43,22 @@ def option1(number):
             bandon.close()
         else: 
             print()
-            print("Uh Oh, Incorrect data entered, please enter '1' or '2'.")
+            print("Uh Oh, incorrect data entered, please enter '1' or '2'.")
             print()
 
-    # elif number == 2:
-    #     print("Adding results for a race...")
-    #     race_name = input("Where did this race take place? >>> ")
-    #     No. racers = int(input("How many racers where in the race?"))
+    elif number == 2:
+        print("Adding results for a race...")
+        race_name = input("Where did this race take place? >>> ")
+        No. racers = int(input("How many racers were in the race?"))
+
+
         
 
     
 def main():
-    number = display_menu()
+    number = display_menu(print_dashes)
     print(number)
-    option1(number)
+    option1(number, print_dashes)
 
 
 main()
